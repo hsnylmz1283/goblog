c2f6e6a62c4aae7fd48134978df587c0c5f64fc3
hsnylmz1283 <23210202015@subu.edu.tr>
2025-07-03 15:32:21 +0300

    head.html eklendi, index.html'de şablon kullanımı başlatıldı.

--- admin/controllers/Dashboard.go
+++ admin/controllers/Dashboard.go
@@ -11,10 +11,10 @@ import (
 type Dashboard struct{}
 
 func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	view, err := template.ParseFiles("admin/views/dashboard/list/index.html")
+	view, err := template.ParseFiles("admin/views/dashboard/list/index.html", "admin/views/templates/head.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	view.Execute(w, nil)
+	view.ExecuteTemplate(w, "index", nil)
 }

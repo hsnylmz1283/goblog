03111ead4d236bec73cdefe23b46490dea585f0c
hsnylmz1283 <23210202015@subu.edu.tr>
2025-07-04 17:49:19 +0300

    Dashboard.go sayfasını yenilemeyi unutmuşum. Önceki committe yapılan değişiklikler hala güncel.

--- admin/controllers/Dashboard.go
+++ admin/controllers/Dashboard.go
@@ -5,8 +5,8 @@ import (
 	"net/http"      //web sunucusu oluşturma, response gönderme gibi işlemleri içerir.
 	"text/template" //HTML Şablonu. .html dosyalarına veri yerleştirip, dinamik içerik üretmeyi sağlar.
 
-	"github.com/hsnylmz1283/goblog/admin/helpers" //El emeği göz nuru, yani BENİM PAKETİM. Dinamik olarak .html dosyalarını çeken yardımcı fonksiyon içeriyor.
-	"github.com/julienschmidt/httprouter"
+	"github.com/hsnylmz1283/goblog/admin/helpers" //Include.go. El emeği göz nuru, yani BENİM PAKETİM. Dinamik olarak .html dosyalarını çeken yardımcı fonksiyon içeriyor.
+	"github.com/julienschmidt/httprouter"         //Yüksek performanslı routing kütüphanesi. URL'den dinamik veri alırız. //tit PORSCHE 911
 )
 
 type Dashboard struct{}

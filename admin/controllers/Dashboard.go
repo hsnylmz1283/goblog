a35218178aa72858a8fecf3e7b10b44334df34fe
hsnylmz1283 <23210202015@subu.edu.tr>
2025-07-04 17:47:08 +0300

    Dashboard için dinamik şablon yükleme sistemi kuruldu. helpers.Include fonksiyonu ile dashboard şablonları ve ortak template'ler birlikte yüklendi.

--- admin/controllers/Dashboard.go
+++ admin/controllers/Dashboard.go
@@ -2,16 +2,17 @@ package controllers
 
 import (
 	"fmt"
-	"net/http"
-	"text/template"
+	"net/http"      //web sunucusu oluşturma, response gönderme gibi işlemleri içerir.
+	"text/template" //HTML Şablonu. .html dosyalarına veri yerleştirip, dinamik içerik üretmeyi sağlar.
 
+	"github.com/hsnylmz1283/goblog/admin/helpers" //El emeği göz nuru, yani BENİM PAKETİM. Dinamik olarak .html dosyalarını çeken yardımcı fonksiyon içeriyor.
 	"github.com/julienschmidt/httprouter"
 )
 
 type Dashboard struct{}
 
 func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	view, err := template.ParseFiles("admin/views/dashboard/list/index.html", "admin/views/templates/head.html")
+	view, err := template.ParseFiles(helpers.Include("dashboard/list")...)
 	if err != nil {
 		fmt.Println(err)
 		return

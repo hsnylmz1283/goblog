cdf8d360012dfcc3f1cd4e331c283b8783344769
hsnylmz1283 <23210202015@subu.edu.tr>
2025-07-25 14:50:36 +0300

    Kayıt ekleme ve listeleme işlemi tamamlandı.

--- admin/controllers/Dashboard.go
+++ admin/controllers/Dashboard.go
@@ -2,11 +2,16 @@ package controllers
 
 import (
 	"fmt"
-	"net/http"      //web sunucusu oluşturma, response gönderme gibi işlemleri içerir.
+	"io"
+	"net/http" //web sunucusu oluşturma, response gönderme gibi işlemleri içerir.
+	"os"
+	"strconv"
 	"text/template" //HTML Şablonu. .html dosyalarına veri yerleştirip, dinamik içerik üretmeyi sağlar.
 
+	"github.com/gosimple/slug"
 	"github.com/hsnylmz1283/goblog/admin/helpers" //Include.go. El emeği göz nuru, yani BENİM PAKETİM. Dinamik olarak .html dosyalarını çeken yardımcı fonksiyon içeriyor.
-	"github.com/julienschmidt/httprouter"         //Yüksek performanslı routing kütüphanesi. URL'den dinamik veri alırız. //tit PORSCHE 911
+	"github.com/hsnylmz1283/goblog/admin/models"
+	"github.com/julienschmidt/httprouter" //Yüksek performanslı routing kütüphanesi. URL'den dinamik veri alırız. //tit PORSCHE 911
 )
 
 type Dashboard struct{}
@@ -17,7 +22,9 @@ func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params 
 		fmt.Println(err)
 		return
 	}
-	view.ExecuteTemplate(w, "index", nil)
+	data := make(map[string]interface{})
+	data["Posts"] = models.Post{}.GetAll()
+	view.ExecuteTemplate(w, "index", data)
 }
 
 func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -27,4 +34,59 @@ func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, param
 		return
 	}
 	view.ExecuteTemplate(w, "index", nil)
+}
+
+func (dashboard Dashboard) Add(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Println("Add fonksiyonu çalıştı")
+	title := r.FormValue("blog-title")
+	slug := slug.Make(title)
+	description := r.FormValue("blog-desc")
+	categoryID, _ := strconv.Atoi(r.FormValue("blog-category"))
+	content := r.FormValue("blog-content")
+
+	//Upload
+	r.ParseMultipartForm(10 << 20)
+	file, header, err := r.FormFile("blog-picture")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f, err := os.OpenFile("uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	//Upload end.
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	models.Post{
+		Title:       title,
+		Slug:        slug,
+		Description: description,
+		CategoryID:  categoryID,
+		Content:     content,
+		Picture_url: "uploads/" + header.Filename,
+	}.Add()
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+	//TODO ALERT OLACAK.
+}
+
+func (dashboard Dashboard) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	post := models.Post{}.Get(params.ByName("id"))
+	post.Delete()
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+}
+
+func (dashboard Dashboard) Edit(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	view, err := template.ParseFiles(helpers.Include("dashboard/edit")...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data := make(map[string]interface{})
+	data["Post"] = models.Post{}.Get(params.ByName("id"))
+	view.ExecuteTemplate(w, "index", data)
 }
--- config/Routes.go
+++ config/Routes.go
@@ -13,8 +13,12 @@ func Routes() *httprouter.Router {
 	//blog posts
 	r.GET("/admin", admin.Dashboard{}.Index)
 	r.GET("/admin/yeni-ekle", admin.Dashboard{}.NewItem)
+	r.POST("/admin/add", admin.Dashboard{}.Add)
+	r.GET("/admin/delete/:id", admin.Dashboard{}.Delete)
+	r.GET("/admin/edit/:id", admin.Dashboard{}.Edit)
 
 	//tit SERVE FILES
 	r.ServeFiles("/admin/assets/*filepath", http.Dir("admin/assets"))
+	r.ServeFiles("/uploads/*filepath", http.Dir("uploads"))
 	return r
 }
